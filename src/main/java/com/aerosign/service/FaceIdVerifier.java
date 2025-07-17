2a1a7908b1ff8182ee9674bdd353dcf28e85cc8c
Валерий Гриднев <vgair@VGAir.local>
2025-07-17 17:54:04 +0300

    SCRUM-9: добавлен FaceIdVerifier для эмуляции биометрии

--- src/main/java/com/aerosign/service/FaceIdVerifier.java
+++ src/main/java/com/aerosign/service/FaceIdVerifier.java
@@ -1,4 +1,17 @@
 package com.aerosign.service;
 
+import com.aerosign.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FaceIdVerifier {
+
+    private static final Logger logger = LoggerFactory.getLogger(FaceIdVerifier.class);
+
+    public boolean verify (User user){
+            logger.info("Эмуляция Face ID: пользователь '{}' прошёл проверку лица", user.getName());
+            return true; // TODO: заменить на реальную проверку в будущем
+    }
 }
